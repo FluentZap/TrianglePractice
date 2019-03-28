5275a4fd5e961395e37f2b115399364e51769b67
hannah melendy, Jake Nagel and Nathan Aden <nathan@>
2019-03-28 16:41:01 -0700

    add angle logic with branches

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   $("#triangle-sides").submit(function(event){
     event.preventDefault();
-    var one = $("#form-side1Input").val();
-    var two = $("#form-side2Input").val();
-    var three = $("#form-side3Input").val();
+    var one = parseFloat($("#form-side1Input").val());
+    var two = parseFloat($("#form-side2Input").val());
+    var three = parseFloat($("#form-side3Input").val());
     var triangle_type = triangle(one, two, three);
     $("#result").text(triangle_type);
   });
@@ -12,7 +12,22 @@ $(document).ready(function(){
 
 
 function triangle(one, two, three) {
-  
+  var isTriangle = (Math.pow(one, 2) + Math.pow(two, 2)) === Math.pow(three, 2);
+  if (!isTriangle) {
+    return "Right angle, right on!"
+  }
+  // {
+  //   return "Not a Triangle, Dude!";
+  // }
+  //All sides Equal
+   if (one === two && two === three) {
+      return "Nice Equilateral Triangle, Bro!";
+    //Isosceles
+  } else if (one === two || one === three || two === three) {
+    return "Nice Isosceles Triangle, Bro!";
+  } else {
+    return "Nice Scalene Triangle, Far out!";
+}
 
 
 
