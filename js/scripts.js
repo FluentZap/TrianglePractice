7a7ac0188abcdefdf41d95a84b6ec5ad5bf1fea8
hannah melendy, Jake Nagel and Nathan Aden <nathan@>
2019-03-28 15:44:33 -0700

    store sides of triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-  $("select#beverage").click(function(){
-    console.log($("#beverage").val());
+  $("#triangle-sides").submit(function(event){
+    event.preventDefault();
+    var one = $("#form-side1Input").val();
+    var two = $("#form-side2Input").val();
+    var three = $("#form-side3Input").val();
+    var triangle_type = triangle(one, two, three);
+    $("#result").text(triangle_type);
   });
+
 });
+
+
+function triangle(one, two, three) {
+  
+
+
+
+  return    //Equilateral, Isosceles, Scalene, Not a Triangle
+}
